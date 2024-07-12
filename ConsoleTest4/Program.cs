8804b2f76c7dd1aff6ef93dd4ef65d7490f64d21
Martin Kramer <martin.kramer@stevanatogroup.com>
2024-07-12 11:20:17 +0200

    OEE stuff

--- ConsoleTest4/Program.cs
+++ ConsoleTest4/Program.cs
@@ -2,6 +2,7 @@
 using DsDbLib.Models;
 using DsDbLib.Helpers;
 using System.Reflection.Emit;
+using System.Xml.Linq;
 
 namespace ConsoleTest4;
 
@@ -9,34 +10,32 @@ public class Program
 {
     static void Main(string[] args)
     {
-        TestDb();
-        //TestDir();
-    }
-    static void PrintRecursive(XElement root)
-    {
-        Console.WriteLine(root.Name);
-        foreach (XElement element in root.Elements())
-        {
-            PrintRecursive(element);
-        }
-    }
 
-    static int PrintAllElements(XElement root)
-    {
-        int count = 0;
-        Stack<XElement> stack = new Stack<XElement>();
-        stack.Push(root);
-        XElement current;
-        while (stack.Count > 0)
-        {
-            current = stack.Pop();
-            count++;
-            Console.WriteLine(current.Name);
-            foreach (var attribute in current.Attributes())
-            {
-                Console.WriteLine("\t" + attribute.Name + "=" + attribute.Value);
-            }
-        }
-        return count;
     }
+}
+public class OeeBatch
+{
+    public double Ppm { get; set; }
+    public int BatchId { get; set; }
+    public string? BatchName { get; set; }
+    public int State { get; set; }
+    public int RecipeId { get; set; }
+    public string? RecipeName { get; set; }
+}
+public class OeeRun
+{
+    public int Status { get; set; }
+}
+
+public class OeeCount
+{
+    public int[]? Counters { get; set; }
+}
+public class OeeAutoEvent : OeeEvent { }
+public class OeeManualEvent : OeeEvent { }
+public class OeeEvent
+{
+    public int Id { get; set; }
+    public DateTime Time { get; set; }
+    public bool Status { get; set; }
 }
