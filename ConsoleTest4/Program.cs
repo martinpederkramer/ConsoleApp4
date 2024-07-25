3c485651e14966be9f95f61fda1ca4fe7e43a501
martin.kramer <mail@martinkramer.dk>
2024-07-25 18:22:30 +0200

    llll

--- ConsoleTest4/Program.cs
+++ ConsoleTest4/Program.cs
@@ -1,31 +1,11 @@
 ﻿using DsDbLib.DataAccess;
+using System.Xml.Linq;
 
 namespace ConsoleTest4;
 public class Program
 {
     static void Main(string[] args)
     {
-        string screenFile = @"D:\temp\xml\HMI_RT_1\screens\un01_em01_Par.xml";
 
-        XDocument doc = XDocument.Load(screenFile);
-        XElement root = doc.Root!;
-        XElement screen = root.Element("Hmi.Screen.Screen")!;
-        PrintAllElements(root);
-    }
-    static void PrintRecursive(XElement root)
-    {
-        Console.WriteLine(root.Name);
-        foreach (XElement element in root.Elements())
-        {
-            Console.WriteLine(machine);
-        }
-    }
-}
-public class Person
-{
-    public string Name { get; set; }
-    public Person()
-    {
-        
     }
 }
