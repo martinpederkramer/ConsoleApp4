b25b4e207a09e0ff17ff91e6840a103ee763d15b
martin.kramer <mail@martinkramer.dk>
2024-08-30 14:22:29 +0200

    Crap

--- ConsoleTest4/Program.cs
+++ ConsoleTest4/Program.cs
@@ -1,4 +1,6 @@
-﻿using DsDbLib.DataAccess;
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using DsDbLib.DataAccess;
 using System.Xml.Linq;
 
 namespace ConsoleTest4;
@@ -6,6 +8,19 @@ public class Program
 {
     static void Main(string[] args)
     {
-
+        BenchmarkRunner.Run<Test>();
+    }
+}
+[MemoryDiagnoser]
+public class Test
+{
+    [Benchmark]
+    public void LoopLines()
+    {
+        int j = 0;
+        for (int i = 0; i < 10; i++)
+        {
+            j+=i;
+        }
     }
 }
